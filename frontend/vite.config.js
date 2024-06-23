ae44c5928df2e4b5fd71f375b85c517abfade877
om prakash sahu <114207732+doni-coder@users.noreply.github.com>
2024-06-23 17:41:26 +0530

    change git config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      '/api': 'http://localhost:3000'
+      '/api': 'https://mern-todo-app-backend-ppfx.onrender.com'
     }
   },
   plugins: [react()],
