c17232319c9ede5d7205f9158d4cf49c503ae4e1
om prakash sahu <114207732+doni-coder@users.noreply.github.com>
2024-06-24 09:04:58 +0530

    cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,10 +4,14 @@ import cors from "cors";
 
 const app = express();
 
-app.use(cors({
-    origin: ["https://mern-todo-app-frontend-i78y.onrender.com"],
-    credentials: true
-}));
+app.use(
+  cors({
+    origin: "https://mern-todo-app-frontend-i78y.onrender.com" ,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
